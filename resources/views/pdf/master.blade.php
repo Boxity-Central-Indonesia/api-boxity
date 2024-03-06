ecf2f05c05880c5b0ce88ee13b76805edd14f0c2
Bintang Tobing <47741235+bintangjtobing@users.noreply.github.com>
2024-03-07 00:55:24 +0700

    update: master pdf

--- resources/views/pdf/master.blade.php
+++ resources/views/pdf/master.blade.php
@@ -1,4 +1,8 @@
-@if (!function_exists('getCompanyName') || !function_exists('getCompanyAddress') || !function_exists('getCompanyPhone') || !function_exists('getCompanyEmail'))
+@if (
+    !function_exists('getCompanyName') ||
+        !function_exists('getCompanyAddress') ||
+        !function_exists('getCompanyPhone') ||
+        !function_exists('getCompanyEmail'))
     @include('app.helpers.helpers')
 @endif
 <!doctype html>
@@ -25,30 +29,30 @@
     </div>
     <hr>
     <h6 class="align-center my-4" style="text-transform: uppercase;">@yield('title')</h2>
-    @yield('content')
-    <footer class="my-5">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 text-left">
-                    Dicetak pada {{ Carbon\Carbon::now()->format('d F Y h:m:s') }}
-                </div>
-                <div class="col-12 text-left">
-                    &copy;PT. Boxity Central Indonesia
+        @yield('content')
+        <footer class="my-5">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 text-left">
+                        Dicetak pada {{ Carbon\Carbon::now()->format('d F Y h:m:s') }}
+                    </div>
+                    <div class="col-12 text-left">
+                        &copy;{{ getCompanyName() }} | All rights reserved by PT. BOXITY CENTRAL INDONESIA.
+                    </div>
                 </div>
             </div>
-        </div>
-    </footer>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
+        </footer>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+        </script>
 </body>
 
 </html>
