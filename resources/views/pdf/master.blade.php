5c946ff38d0384cf7d15de1c6125ad2bd988bc9f
Bintang Tobing <47741235+bintangjtobing@users.noreply.github.com>
2024-03-10 13:37:26 +0700

    fix: waktu cetak

--- resources/views/pdf/master.blade.php
+++ resources/views/pdf/master.blade.php
@@ -28,33 +28,35 @@
         </div>
     </div>
     <hr>
-    <h6 class="align-center my-4" style="text-transform: uppercase;">@yield('title')</h2>
-        @yield('content')
-        <footer class="my-5">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12 text-left">
-                        Dicetak pada {{ Carbon\Carbon::now()->format('d F Y h:m:s') }}
-                    </div>
-                    <div class="col-12 text-left">
-                        &copy;Copyright {{ Carbon\Carbon::now()->format('Y') }}&nbsp;{{ getCompanyName() }}. All rights
-                        reserved by <abbr title="PT Boxity Central Indonesia"><a href="https://boxity.id"
-                                target="_blank">BoxityID</a></abbr>.
-                    </div>
+    <h5 class="align-center mt-4" style="text-transform: uppercase;">@yield('title')</h5>
+    <p class="align-center" style="text-transform: capitalize;">Waktu Cetak:
+        {{ Carbon\Carbon::now()->translatedFormat('d F Y h:m:s') }}</p>
+    @yield('content')
+    <footer class="my-5">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 text-left">
+                    Dicetak pada {{ Carbon\Carbon::now()->translatedFormat('d F Y h:m:s') }}
+                </div>
+                <div class="col-12 text-left">
+                    &copy;Copyright {{ Carbon\Carbon::now()->format('Y') }}&nbsp;{{ getCompanyName() }}. All rights
+                    reserved by <abbr title="PT Boxity Central Indonesia"><a href="https://boxity.id"
+                            target="_blank">BoxityID</a></abbr>.
                 </div>
             </div>
-        </footer>
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-        </script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-        </script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-        </script>
+        </div>
+    </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
