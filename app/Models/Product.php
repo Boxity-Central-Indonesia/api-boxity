e8888591f60be730dc98e4ca85ae52c572489a50
Bintang Tobing <47741235+bintangjtobing@users.noreply.github.com>
2024-05-16 14:08:34 +0800

    fix: delete limit str on products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -35,11 +35,6 @@ class Product extends Model
     {
         return $image_product ? $image_product : 'https://res.cloudinary.com/boxity-id/image/upload/v1709745192/39b09e1f-0446-4f78-bbf1-6d52d4e7e4df.png';
     }
-    public function getDescriptionAttribute($description)
-    {
-        // Limit the description to 150 characters
-        return \Str::limit($description, 30, '...');
-    }
     public function category()
     {
         return $this->belongsTo(ProductsCategory::class);
